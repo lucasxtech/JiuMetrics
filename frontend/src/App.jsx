4f94f3a108accc05be51771094eb4dc6d6cc5fd2
lucasxtech <menezeslucas500@gmail.com>
2025-12-11 19:46:04 -0300

    fix: add basename to BrowserRouter for GitHub Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -48,7 +48,7 @@ function AppContent() {
 
 export default function App() {
   return (
-    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <BrowserRouter basename="/JiuMetrics" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <AppContent />
     </BrowserRouter>
   );
